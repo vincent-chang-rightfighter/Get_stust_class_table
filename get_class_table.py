a030be95f6d8b7b99b8575d4d679ec37efefc799
vincentchangtw <vincent870922@yahoo.com.tw>
2021-01-28 21:28:36 +0800

    add get_clas_table.py

---
+++ get_class_table.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+import os
+os.getcwd() 
+
+with requests.Session() as s:
+    page = s.get('https://course.stust.edu.tw/CourSel/Login.aspx?ReturnUrl=%2fCourSel%2fboard.aspx')
+    soup = BeautifulSoup(page.content, 'lxml')
+
+    payload_loginPage = {
+        'Login1$UserName': 'sutdent_id',
+        'Login1$Password': 'student_password',
+        'Login1$LoginButton': '登入'
+    }
+
+    payload_loginPage["__VIEWSTATE"] = soup.select_one("#__VIEWSTATE")["value"]
+    payload_loginPage["__VIEWSTATEGENERATOR"] = soup.select_one("#__VIEWSTATEGENERATOR")["value"]
+    payload_loginPage["__EVENTVALIDATION"] = soup.select_one("#__EVENTVALIDATION")["value"]
+    s.post('https://course.stust.edu.tw/CourSel/Login.aspx?ReturnUrl=%2fCourSel%2fboard.aspx', data=payload_loginPage)
+
+page = s.get('https://course.stust.edu.tw/CourSel/Pages/MyTimeTable.aspx?role=S')
+soup = BeautifulSoup(page.content, 'html.parser')
+table = pd.read_html(page.text, encoding='utf-8',keep_default_na=False)[31]
+#table=table.drop(table.index[10])
+table=table.drop([9])
+#table.style.hide_index()
+table.to_csv('Result1.csv',index=0,encoding='utf-8-sig')
+print("finish check file")
